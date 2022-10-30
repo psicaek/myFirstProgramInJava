d13b56c220e45fa41a56c9170b2853f219e942b0
psicaek <111680051+psicaek@users.noreply.github.com>
2022-10-30 12:54:26 +0100

    If else statement!!

--- src/chapter3/QuotaCalculator.java
+++ src/chapter3/QuotaCalculator.java
@@ -11,21 +11,21 @@ for those who dont they are informed of how many sales they where short
 public class QuotaCalculator {
 
 public static void main(String []args){
-    //Get unknown values
- System.out.println("how many sales:?");
-    Scanner scanner = new Scanner(System.in);
-    int sales = scanner.nextInt();
-    Scanner.close();
-    //Initialise values we know
-    int mustSales=10;
+        //Get unknown values
+    System.out.println("how many sales:?");
+        Scanner scanner = new Scanner(System.in);
+        int sales = scanner.nextInt();
+        scanner.close();
+        //Initialise values we know
+            int mustSales=10;
 
-    //Make a decision ont the path to take- output
-    if (sales>=10){
+        //Make a decision ont the path to take- output
+        if (sales>=10){
         System.out.println("well done you made the extra sales!!!");
-    }
-    else {
+        }
+        else {
         int salesMin = mustSales-sales;
         System.out.println("You where short "+salesMin+" sales!");
+        }
     }
-}
 }
